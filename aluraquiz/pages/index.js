a563ccec7ecd74ab15af0d2b63abea517b0c0ad6
rafaelalbia <rafaelalbia@outlook.com>
2021-02-03 11:01:22 -0300

    Components import

--- aluraquiz/pages/index.js
+++ aluraquiz/pages/index.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components'
 import db from "../db.json"
 import Widget from "../src/components/Widget"
+import QuizBackground from "../src/components/QuizBackground"
+import Footer from "../src/components/Footer"
+import GitHubCorner from "../src/components/GitHubCorner"
 
-const BackgroundImage = styled.div`
-  background-image: url(${db.bg});
-  flex: 1;
-  background-size: cover;
-  background-position: center;
-`;
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+// `;
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -22,7 +25,7 @@ export const QuizContainer = styled.div`
 
 export default function Home() {
   return (
-    <BackgroundImage>
+    <QuizBackground backgroundImage={db.bg}>
       <QuizContainer>
         <Widget>
           <Widget.Header>
@@ -35,11 +38,15 @@ export default function Home() {
         </Widget>
           
         <Widget>
-          <h1>Quizes of the community</h1>
-
-          <p>Lorem inpsum dolor sit amet...</p>
+          <Widget.Content>
+            <h1>Quizes of the community</h1>
+  
+            <p>Lorem inpsum dolor sit amet...</p>
+          </Widget.Content>
         </Widget>
+        <Footer />
       </QuizContainer>
-    </BackgroundImage>
+      <GitHubCorner projectUrl="https://github.com/rafaelalbia"/>
+    </QuizBackground>
   );
 }
